307467e718f810fb8adbede8eef21fdbd23d2482
Salma_KR <48034001+SalmaKazemiRashed@users.noreply.github.com>
2026-04-02 12:40:03 +0200

    change neo4j client.py using dotenv

--- neo4j_client.py
+++ neo4j_client.py
@@ -1,11 +1,25 @@
+
 from neo4j import GraphDatabase
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env including pass and user name
+load_dotenv() 
+
+print(os.getenv("NEO4J_URI"))
+print(os.getenv("NEO4J_USER"))
+print(os.getenv("NEO4J_PASSWORD"))
+
 
 class Neo4jClient:
 
     def __init__(self):
         self.driver = GraphDatabase.driver(
-            "bolt://localhost:7687",
-            auth=("neo4j", "password")
+            os.getenv("NEO4J_URI"),
+            auth=(
+                os.getenv("NEO4J_USER"),
+                os.getenv("NEO4J_PASSWORD")
+            )
         )
 
     def run(self, query, params=None):
